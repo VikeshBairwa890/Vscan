7bd33922db795f73f41d8bf7e4ba86df2fe6004d
vikesh <vikeshbairwa890@gmail.com>
2026-07-10 22:23:00 +0530

    feat: initialize Prisma client with Postgres connection pool and adapter

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -15,7 +15,7 @@ import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../generated/prisma/client'
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.vscan_DATABASE_URL || process.env.DATABASE_URL
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
